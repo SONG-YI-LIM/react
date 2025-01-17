2993f50b47ae6bd290de4d6a3666a472ab930790
SONG-YI-LIM <iamlsy.da@gmail.com>
2025-01-17 15:05:30 +0900

    2025-01-17
    문제 1 complete

---
+++ practice-app/src/screens/components/comp-answer01-lsy.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useState, useEffect, useRef} from 'react';
+import styled from 'styled-components';
+
+const HelloDiv = styled.div`
+    width: 50px;
+    height: 50px;
+    background: red;
+`
+
+export const CompAnswer01Lsy = () => {
+    // 출력
+    return (
+        <HelloDiv />
+    );
+};
--- practice-app/src/screens/page-practice.tsx
+++ practice-app/src/screens/page-practice.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState, useEffect, useRef} from 'react';
-import {CompAnswer01, CompAnswer02, CompAnswer03} from '@/screens'
+import {CompAnswer01, CompAnswer02, CompAnswer03, CompAnswer01Lsy} from '@/screens';
 
 export const PagePractice = () =>{
 
@@ -10,6 +10,7 @@ export const PagePractice = () =>{
                 <h5 className="tit">문제 1</h5>
                 <p className="txt">배경색 red값을 가진 스타일 컴포넌트를 만드세요</p>
                 <p className="txt">답변 : </p>
+                <CompAnswer01Lsy />
                 {/* 이 위치에 컴포넌트를 마운트 */}
                 <CompAnswer01 />
             </div>
