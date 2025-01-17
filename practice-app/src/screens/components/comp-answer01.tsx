9450c730f68a61a4b7e47a22deaf90e701bf1906
mn4563210 <mn4563210@gmail.com>
2025-01-17 09:46:10 +0900

    fix: 답변 주석 추가

--- practice-app/src/screens/components/comp-answer01.tsx
+++ practice-app/src/screens/components/comp-answer01.tsx
@@ -18,33 +18,25 @@ export const CompAnswer01 = () =>{
                 <div className="answer-wrap">
                     <ColorRed />
                     <div className="example">
-                    <p>import React, {'{ useState }'} from 'react';</p>
-                    <p>import styled from 'styled-components';</p>
+                        <p className="cGray">// 스타일 컴포넌트 연결</p>
+                        <p>import styled from 'styled-components';</p>
+                        <br/>
+                        <p>export const CompAnswer01 = () =&gt; &#123;</p>
+                        <br/>
+                        <p className="cGray">// div 스타일 컴포넌트 생성</p>
+                        <p className="indent-1">const ColorRed = styled.div`</p>
+                        <p className="indent-2">width: 100px;</p>
+                        <p className="indent-2">height: 100px;</p>
+                        <p className="indent-2">background: red;</p>
+                        <p className="indent-1">`;</p>
+                        <br/>
+                        <p className="cGray">// 출력</p>
+                        <p className="indent-1">return (</p>
 
-                    <p>export const CompAnswer01 = () =&gt; &#123;</p>
+                        <p className="indent-5">&lt;ColorRed /&gt;</p>
 
-                    <p className="indent-1">const [toggle, setToggle] = useState(false);</p>
-
-                    <p className="indent-1">const ColorRed = styled.div`</p>
-                    <p className="indent-2">width: 100px;</p>
-                    <p className="indent-2">height: 100px;</p>
-                    <p className="indent-2">background: red;</p>
-                    <p className="indent-1">`;</p>
-
-                    <p className="indent-1">return (</p>
-                    <p className="indent-2">&lt;&gt;</p>
-                    <p className="indent-3">&lt;button class="btn" onClick=&#123;() =&gt; setToggle(!toggle)&#125;&gt;답변 보기&lt;/button&gt;</p>
-
-                    <p className="indent-3">&#123;toggle ? (</p>
-                    <p className="indent-4">&lt;div class="answer-wrap"&gt;</p>
-                    <p className="indent-5">&lt;ColorRed /&gt;</p>
-                    <p className="indent-5">&lt;div class="example"&gt;&lt;/div&gt;</p>
-                    <p className="indent-4">&lt;/div&gt;</p>
-                    <p className="indent-3">) : null&#125;</p>
-
-                    <p className="indent-2">&lt;/&gt;</p>
-                    <p className="indent-1">);</p>
-                    <p>&#125;;</p>
+                        <p className="indent-1">);</p>
+                        <p>&#125;;</p>
 
                     </div>
                 </div>
